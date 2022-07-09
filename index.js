904ace48b6f4c5c8c9bc7cb083bde3555c67cc60
Gucci Fan <gucc@umich.edu>
2022-07-09 19:37:36 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//const titleCased = () => {
+//  return tutorials
+//}
+
+/*function titleCased(){
+  const justOne = tutorials.map(
+    function(tutorial){
+      tutorial.toLowerCase().split(' ')
+      for (let i=0; i<tutorial.length; i++){
+        tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1)
+      }
+      return tutorial.join(' ')
+    })
+  }*/
+
 const titleCased = () => {
-  return tutorials
+  tutorials.map((tutorial)=>{
+  const newTutorial = []
+  newTutorial.push(tutorial.split(' ').map((word) => word[0].toUpperCase()+word.slice(1)))
+    console.log(newTutorial.join(' '))
+    return newTutorial
+  })
 }
